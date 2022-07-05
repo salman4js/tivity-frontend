d4d7d679a341ccc51758b5cb0c8883b54ae9d9e3
Salman4Js <92971152+salman4js@users.noreply.github.com>
2022-07-05 10:24:03 +0530

    Update Home2.js

--- Screens/Home2.js
+++ Screens/Home2.js
@@ -15,7 +15,7 @@ const Home2 = (props) => {
     const navigation = useNavigation()
 
     const getNotes = () => {
-        axios.post(`https://62c1dbbb2a7fa33f7707dd52--luminous-melba-d1742f.netlify.app/.netlify/functions/server/${props.user}/allnotes`)
+        axios.post(`${host}/${props.user}/allnotes`)
         .then(res => setNotes(res.data))
     }
 
